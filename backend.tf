0a613de1e84cdf81e7f0e6bf6783a1368c84d262
Nice <obiasedos@yahoo.com>
2026-05-20 11:12:24 +0100

    PR pipeline

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "nickos-bucket"
-    key            = "prod/terraform.tfstate"
+    key            = "prod-statesfile/terraform.tfstate"
     region         = "eu-west-2"
     use_lockfile   =  false
     encrypt        = true
